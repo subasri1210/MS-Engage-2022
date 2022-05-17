0456494c9f9981b9b5ddcc9f9bd162fb18ed7b39
suba1210 <subasris1210@gmail.com>
2022-05-17 16:12:11 +0530

    feat(ui): nav

---
+++ client/src/components/NavBar/Nav.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import {
+  chakra,
+  Box,
+  Flex,
+  useColorModeValue,
+  HStack,
+  Button,
+  useDisclosure,
+  VStack,
+  IconButton,
+  CloseButton,
+  useColorMode
+} from '@chakra-ui/react';
+import { BsEmojiSmile } from 'react-icons/bs';
+import { AiOutlineMenu, AiOutlineUser } from 'react-icons/ai';
+import { FiSun, FiMoon, FiLogOut } from 'react-icons/fi';
+
+export default function Gslr() {
+  const bg = useColorModeValue('white', 'gray.800');
+  const mobileNav = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <chakra.header
+      bg={bg}
+      w="full"
+      px={{ base: 2, sm: 4 }}
+      py={4}
+      shadow="md"
+    >
+      <Flex alignItems="center" justifyContent="space-between" mx="auto">
+        <Flex>
+          <chakra.a
+            href="/"
+            title="Choc Home Page"
+            display="flex"
+            alignItems="center"
+          >
+            <BsEmojiSmile size={30} />
+          </chakra.a>
+          <chakra.h1 fontSize="3xl" fontWeight={500} ml="2">
+            FaceFirst
+          </chakra.h1>
+        </Flex>
+        <HStack display="flex" alignItems="center" spacing={1}>
+          <HStack
+            spacing={5}
+            mr={1}
+            display={{ base: 'none', md: 'inline-flex' }}
+          >
+            <Button fontSize={20} fontWeight={300} variant="ghost">Organizations</Button>
+            <IconButton
+              onClick={toggleColorMode}
+              icon={colorMode === 'dark' ? <FiSun /> : <FiMoon />}
+            />
+            <IconButton
+              icon={<AiOutlineUser />}
+            />
+            <IconButton
+              icon={<FiLogOut />}
+            />
+          </HStack>
+          <Box display={{ base: 'inline-flex', md: 'none' }}>
+            <IconButton
+              display={{ base: 'flex', md: 'none' }}
+              aria-label="Open menu"
+              fontSize="20px"
+              color={useColorModeValue('gray.800', 'inherit')}
+              variant="ghost"
+              icon={<AiOutlineMenu />}
+              onClick={mobileNav.onOpen}
+            />
+            <VStack
+              pos="absolute"
+              top={0}
+              left={0}
+              right={0}
+              display={mobileNav.isOpen ? 'flex' : 'none'}
+              flexDirection="column"
+              p={2}
+              pb={4}
+              m={2}
+              bg={bg}
+              spacing={3}
+              rounded="sm"
+              shadow="sm"
+            >
+              <CloseButton
+                aria-label="Close menu"
+                onClick={mobileNav.onClose}
+              />
+              <Button w="full" variant="ghost">
+                Organisation
+              </Button>
+              <Button w="full" variant="ghost" onClick={toggleColorMode}>
+                Switch to
+                {' '}
+                {colorMode === 'dark' ? 'light' : 'dark'}
+                {' '}
+                mode
+              </Button>
+              <Button w="full" variant="ghost">
+                Profile
+              </Button>
+              <Button w="full" variant="ghost">
+                Logout
+              </Button>
+            </VStack>
+          </Box>
+        </HStack>
+      </Flex>
+    </chakra.header>
+  );
+}
